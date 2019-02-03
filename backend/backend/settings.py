640f968413be8f454516334a20757a40ec54a92c
oconnorssoftware <joshoconnor7113@gmail.com>
2019-02-03 10:18:38 -0500

    fixed settings so that only email and pw are required for reg and login

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -133,3 +133,15 @@ STATICFILES_DIRS = [
 STATIC_URL = '/static/'
 SITE_ID = 1
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_EMAIL_REQUIRED = True   
+ACCOUNT_USERNAME_REQUIRED = False
+
+#Following is added to enable registration with email instead of username
+AUTHENTICATION_BACKENDS = (
+ # Needed to login by username in Django admin, regardless of `allauth`
+ "django.contrib.auth.backends.ModelBackend",
+
+ # `allauth` specific authentication methods, such as login by e-mail
+ "allauth.account.auth_backends.AuthenticationBackend",
+)
